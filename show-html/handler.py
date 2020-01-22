2b43107bc15aad79f4458524980fd9f52506c214
dmcneill <dmcneill@smartfocus.com>
2020-01-22 16:03:39 +0000

    test new stack

---
+++ show-html/handler.py
@@ -0,0 +1,9 @@
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+
+    html = '<html><h2>Hi, from your function!</h2></html>'
+
+    return html
